3126616e0951a35558b07df510c8d22ecc02d8e4
Linos Gian <Ragan@192.168.1.13>
2016-07-31 16:11:18 +0300

    Added url rule to feedthe dynamic thumbnails for Posts' creation

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from . import views
 
-
 app_name = 'main'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^articles/(?P<post_id>[0-9]+)$', views.articles, name='articles'),
     url(r'^categories/(?P<category_id>[0-9]+)$', views.index, name='categories'),
     url(r'^about/$', views.about, name='about'),
+    url(r'^ajax/$', views.feed_ajax, name='ajax'),
 ]
