5a4276bd9e84b4f8e5a4fa32a1c642b1c0b4a1ca
linosgian <vaggian1992@gmail.com>
2016-08-22 01:59:23 +0300

    Finalized styles and fixed hardcoded stuff.

--- main/static/main/assets/js/thumbnails.js
+++ main/static/main/assets/js/thumbnails.js
@@ -5,26 +5,23 @@ $(document).ready( function ()
 	{
 		var option = $ (this).find('option:selected').text()
 		if (option !== "---------" ){ 
-			// TODO: Change that when we upload the server. Add domain name instead of ip!
-			$.ajax({url:"https://139.59.175.70/ajax?title="+option,
-			            success:function(result) {
-							if ($('#thumb').length){
-								$('#thumb').attr('src',"https://139.59.175.70"+result);
-			            	}
-			            	else
-			            	{
-	            				$('<img src="'+result+'" id="thumb" style="max-width:150px; height:auto; max-height:150px;"/>').appendTo(".grp-readonly");
-
-			            	}
-			            },
-			            error:function(xhr, status, error) {
-			                
-			            },
-			            dataType:"text",
-				    crossDomain: true,
-			            type:"get"
-			        }
-			    );
+			$.ajax({
+				type: "GET",
+				url:"/ajax?title="+option,
+	            success:function(result) {
+					if ($('#thumb').length){
+						$('#thumb').attr('src',result);
+	            	}
+	            	else
+	            	{
+        				$('<img src="'+result+'" id="thumb" style="max-width:150px; height:auto; max-height:150px;"/>').appendTo(".grp-readonly");
+	            	}
+	            },
+	            error:function(xhr, status, error) {	                
+	            },
+	            dataType:"text",
+			    crossDomain: true,
+			});
 		}
 	});
 });
